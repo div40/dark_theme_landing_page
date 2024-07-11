55adff0e5960e8e4c8b6e33b5a667ec327ea074f
div40 <vidh40@gmail.com>
2024-07-11 15:02:06 +0530

    hero

---
+++ src/components/Hero.tsx
@@ -0,0 +1,62 @@
+import Link from "next/link";
+import ArrowIcon from "../assets/icons/arrow-w.svg";
+import cursorImage from "../assets/images/cursor.png";
+import messageImage from "../assets/images/message.png";
+import Image from "next/image";
+
+export const Hero = () => {
+  return (
+    <div className="bg-black text-white bg-[linear-gradient(to_bottom,#000,#200D42_34%,#4F21A1_65%,#A46EDB_82%)] py-[72px] sm:py-24 relative overflow-clip">
+      <div className="absolute h-[375px] w-[750px] sm:w-[1536px] sm:h-[768px] lg:w-[2500px] lg:h-[1200px] rounded-[100%] bg-black left-1/2 -translate-x-1/2 border border-[#B48CDE] bg-[radial-gradient(closest-side,#000_82%,#9560EB)] top-[calc(100%-96px)] sm:top-[calc(100%-120px)]"></div>
+      <div className="container relative">
+        <div className="flex items-center justify-center">
+          <Link
+            href={"#"}
+            className="py-1 border px-2 rounded-lg border-white/30 inline-flex gap-3"
+          >
+            <span className="bg-[linear-gradient(to_right,#F87AFF,#FB93D0,#FFDD99,#C3F0B2,#2FD8FE)] text-transparent bg-clip-text [-webkit-background-clip:text]">
+              Version 2.0 is here!
+            </span>
+            <span className="flex items-center gap-1">
+              <span>Read More</span>
+              <ArrowIcon />
+            </span>
+          </Link>
+        </div>
+        <div className="flex justify-center mt-8">
+          <div className="inline-flex relative">
+            <h1 className="text-7xl sm:text-9xl font-bold tracking-tighter text-center  inline-flex">
+              One Task <br /> at a Time
+            </h1>
+            <Image
+              src={cursorImage}
+              alt=""
+              height={200}
+              width={200}
+              className="absolute right-[476px] top-[108px] hidden sm:inline"
+            />
+            <Image
+              src={messageImage}
+              alt=""
+              height={200}
+              width={200}
+              className="absolute top-[56px] left-[498px] hidden sm:inline"
+            />
+          </div>
+        </div>
+        <div className="flex justify-center">
+          <p className="text-center text-xl mt-8 max-w-md">
+            Say goodbye to ugly todos. We are a next generation task management
+            app.It's intuitive, visually stunning, and engineered from the
+            ground up to boost your productivity.
+          </p>
+        </div>
+        <div className="flex justify-center mt-8">
+          <button className="bg-white text-black py-3 px-5 rounded-lg font-medium">
+            Get for free
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
