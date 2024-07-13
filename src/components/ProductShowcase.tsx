92cf89a8af3d74b83eb0f2f186964634dca7191d
div40 <vidh40@gmail.com>
2024-07-13 15:11:53 +0530

    product showcase animation

--- src/components/ProductShowcase.tsx
+++ src/components/ProductShowcase.tsx
@@ -1,7 +1,23 @@
+"use client";
 import Image from "next/image";
 import productScreenshot from "../assets/images/app-screen.png";
+import { motion, useScroll, useTransform } from "framer-motion";
+import { useRef } from "react";
 
 export const ProductShowcase = () => {
+  const productImage = useRef<HTMLImageElement>(null);
+
+  // offset explanation :
+  // start of the image reaches end of the page
+  // end of the image reaches end of the page
+
+  const { scrollYProgress } = useScroll({
+    target: productImage,
+    offset: ["start end", "end end"],
+  });
+
+  const rotateX = useTransform(scrollYProgress, [0, 1], [15, 0]);
+  const opacity = useTransform(scrollYProgress, [0, 1], [0.2, 1]);
   return (
     <div className="bg-black text-white bg-gradient-to-b from-black to-[#5D2CA8] py-[72px] sm:py-24">
       <div className="container">
@@ -14,11 +30,20 @@ export const ProductShowcase = () => {
             harum iusto. Et, alias a! Minima architecto tempore doloribus quas.
           </p>
         </div>
-        <Image
-          src={productScreenshot}
-          alt="product screenshot"
-          className="mt-14 mx-auto"
-        />
+        <motion.div
+          style={{
+            opacity: opacity,
+            rotateX: rotateX,
+            transformPerspective: "800px",
+          }}
+        >
+          <Image
+            ref={productImage}
+            src={productScreenshot}
+            alt="product screenshot"
+            className="mt-14 mx-auto"
+          />
+        </motion.div>
       </div>
     </div>
   );
